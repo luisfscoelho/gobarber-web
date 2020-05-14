c6cc89c319b91539f54bcea3a9a212f882ccf9ea
Luis Coelho <luis.fs.coelho@pm.me>
2020-05-14 20:31:38 -0300

    fix display

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -60,7 +60,7 @@ export const Content = styled.div`
 `;
 
 export const Background = styled.div`
-  flex: 1%;
+  flex: 1;
   background: url(${signInBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
