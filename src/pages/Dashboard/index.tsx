b306911db385db307da831efc52f5302992f8cce
Luis Coelho <luis.fs.coelho@pm.me>
2020-08-18 20:39:29 -0300

    Página de perfil

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import { isToday, format, parseISO, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import { FiPower, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import 'react-day-picker/lib/style.css';
 
 import { useAuth } from '../../hooks/auth';
@@ -141,7 +142,9 @@ const Dashboard: React.FC = () => {
 
             <div>
               <span>Bem vindo(a),</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
